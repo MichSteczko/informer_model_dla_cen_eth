1896d3ada996f91d42e5bc596f3f761be5bd274c
Michał Steczko <michalo.steczeko@gmail.com>
2022-09-04 15:03:03 +0200

    scraper py has been updated to the latest version and ethereum prices historical dataset has been created

--- price_predictor/scraper_danych/scraper.py
+++ price_predictor/scraper_danych/scraper.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from dataclasses import dataclass
 import numpy as np
 
+
 @dataclass
 class ScrapeCryptoPrice:
     start_date: datetime.datetime
@@ -23,9 +24,8 @@ class ScrapeCryptoPrice:
         end_date_in_unix_format = int(time.mktime(end_date.timetuple()))
         return (str(start_date_in_unix_format), str(end_date_in_unix_format))
 
-
-    def _get_unix_timestamps_from_time_period(self,
-        start_date: datetime.datetime, end_date: datetime.datetime
+    def _get_unix_timestamps_from_time_period(
+        self, start_date: datetime.datetime, end_date: datetime.datetime
     ) -> list:
         """Ta funckja jest odpowiedzialna za utworzenie dat
         w odstępach 90 dniowych w podanym okresie czasu"""
@@ -46,8 +46,8 @@ class ScrapeCryptoPrice:
         ]
         return ninety_days_unix_timestamps
 
-
-    def _get_api_link(self, 
+    def _get_api_link(
+        self,
         start_unix_timestamp: str,
         end_unix_timestamp: str,
         token_id: str = "ethereum",
@@ -56,7 +56,6 @@ class ScrapeCryptoPrice:
         """Ta funkcja tworzy link, który zostanie wykorzystany do komunikacji z API"""
         return f"https://api.coingecko.com/api/v3/coins/{token_id}/market_chart/range?vs_currency={currency}&from={start_unix_timestamp}&to={end_unix_timestamp}"
 
-
     def get_token_prices(self):
         """Ta funckja zwraca ceny wybranej kryptowaluty w określonym okresie czasu"""
         if self.api_links:
@@ -64,7 +63,7 @@ class ScrapeCryptoPrice:
             for api_link in self.api_links:
                 with requests.Session() as session:
                     adapter = requests.adapters.HTTPAdapter(max_retries=5)
-                    session.mount('https://', adapter)
+                    session.mount("https://", adapter)
                     json_response = session.get(api_link).json()["prices"]
                     prices.append(json_response)
             return np.concatenate(prices)
@@ -73,7 +72,9 @@ class ScrapeCryptoPrice:
     def get_api_links(self):
         """Funkcja pobierająca ceny rynkowe danej kryptowaluty, w danym przedziale czasu"""
         api_links = []
-        unix_intervals = self._get_unix_timestamps_from_time_period(self.start_date, self.end_date)
+        unix_intervals = self._get_unix_timestamps_from_time_period(
+            self.start_date, self.end_date
+        )
         for start_unix_timestamp, end_unix_timestamp in unix_intervals:
             api_link = self._get_api_link(
                 start_unix_timestamp=start_unix_timestamp,
@@ -87,17 +88,17 @@ class ScrapeCryptoPrice:
 
 
 def save_token_historical_data(price_list, token_name):
-    price_dataframe = pd.DataFrame(price_list, columns=['Timestamp', 'Price'])
+    price_dataframe = pd.DataFrame(price_list, columns=["Timestamp", "Price"])
     directory_path = Path(__file__).resolve().parents[1]
     saving_path = os.path.join(directory_path, "dane", f"{token_name}_prices.csv")
     price_dataframe.to_csv(saving_path)
-    return price_dataframe    
+    return price_dataframe
 
 
-date_a = datetime.datetime(2017, 1, 1, 00, 1)
-date_b = datetime.datetime(2022, 8, 30, 00, 1)
-elko = ScrapeCryptoPrice(date_a, date_b, "ethereum", "usd").get_api_links()
-witam = save_token_historical_data(elko.get_token_prices(), "ethereum")
-print(witam)
-print("unix_timestamp => ", (type(time.mktime(date_b.timetuple()))))
-
+if __name__ == "main":
+    date_a = datetime.datetime(2017, 1, 1, 00, 1)
+    date_b = datetime.datetime(2022, 8, 30, 00, 1)
+    api_links = ScrapeCryptoPrice(date_a, date_b, "ethereum", "usd").get_api_links()
+    create_ethereum_prices_dataset = save_token_historical_data(
+        elko.get_token_prices(), "ethereum"
+    )
--- scraper_danych/check.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-d = Path(__file__).resolve().parents[1]
-print(d)
